b62cd07dbc764a848a2eecac1e4d267eec252d78
jeffry1829 <jeffry1829@gmail.com>
2016-04-15 23:52:21 +0800

    Create petjelinux.js

---
+++ petjelinux.js
@@ -0,0 +1,13 @@
+{
+  "id": "petjelinux",
+  "alternate": ["petjelinux_","javabot","ysitd_javabot","petjelinux__"],
+  "nickname": "petjelinux",
+  "website": "http://petjelinux.clouds.twgogo.org/wordpress/",
+  "facebook": "100006648784102",
+  "plurk": undefined,
+  "twitter": undefined,
+  "github": "https://github.com/jeffry1829",
+  "email": ["jeffry1829@gmail.com"],
+  "isBot": false,
+  "owner": "petjelinux"
+}
